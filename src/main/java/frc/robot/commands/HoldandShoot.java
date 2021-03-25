1bda0459b40d4e25b00580d70ea3067b7c7e498e
arnoldtkalmbach <35353916+arnoldtkalmbach@users.noreply.github.com>
2021-03-25 13:26:41 -0400

    add parallel command to hold aim point and shoot.  add all stop command, neither mapped to joystick button

---
+++ src/main/java/frc/robot/commands/HoldandShoot.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+import frc.robot.subsystems.ShooterSub;
+
+import static frc.robot.Constants.*;
+
+
+public class HoldandShoot extends ParallelCommandGroup {
+   /** Creates a new Aim. */
+   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+   private final ShooterSub m_subsystem;
+
+  /** Creates a new HoldandShoot. */
+  public HoldandShoot(ShooterSub subsystem) {
+    m_subsystem = subsystem;
+
+      // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_subsystem);
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(new AimHold(m_subsystem), 
+                new Shoot(m_subsystem));
+  }
+}
